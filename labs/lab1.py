4241239bab755a7574b0fc2348b28eff4d40e4ea
MrFarost <144201962+MrFarost@users.noreply.github.com>
2023-09-20 11:13:04 +0500

    Update lab1.py
    Первое задание

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,3 +1,9 @@
 import math
-def task(1)
- pass
+//task1
+input (x=1)
+input (y=8)
+input (z=6)
+input (a)
+input (b)
+a=(np.sin(x)**2)+(np.cos(y)**2)+(ln(np.e**x + np.e**(-y)))
+b=(ln(np.sqrt(X-1)))
