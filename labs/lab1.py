a037c39487ad63adb1d234e73e1c91f54a4a19b0
MrFarost <144201962+MrFarost@users.noreply.github.com>
2023-09-27 11:12:38 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -7,4 +7,4 @@ a=(math.sin(x)**2)+(math.cos(y)**2)+(math.log(math.e**x + math.e**(-y),math.e))
 b=(math.log(math.sqrt(math.fabs(x-1)),math.e))-(pow(5,1/math.fabs(5)))
 print(a)
 print(b)
-#task2
+#task2sssssssss
