94b97fdbc8aaab30913f7d264e2f418dfd9d6a86
MrFarost <144201962+MrFarost@users.noreply.github.com>
2023-09-27 11:04:49 +0500

    Update lab1.py
    no problem task 1

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,11 +1,10 @@
 import math
-//task1
-input (x=1)
-input (y=8)
-input (z=6)
-input (a)
-input (b)
-a=(math.sin(x)**2)+(math.cos(y)**2)+(ln(math.e**x + np.e**(-y)))
-b=(ln(math.sqrt|X-1|))-(pow(5,1/|5|))
-Print(a,b)
-//task2
+#task1
+x=1
+y=8
+z=6
+a=(math.sin(x)**2)+(math.cos(y)**2)+(math.log(math.e**x + math.e**(-y),math.e))
+b=(math.log(math.sqrt(math.fabs(x-1)),math.e))-(pow(5,1/math.fabs(5)))
+print(a)
+print(b)
+#task2
