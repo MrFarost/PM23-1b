6b91fce47e9a959c9fac84b7c894a02f27490011
MrFarost <144201962+MrFarost@users.noreply.github.com>
2023-10-11 10:45:14 +0500

    Update lab1.py
    Fix первых трёх заданий

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,10 +1,29 @@
 import math
-#task1
-x=1
-y=8
-z=6
-a=(math.sin(x)**2)+(math.cos(y)**2)+(math.log(math.e**x + math.e**(-y),math.e))
-b=(math.log(math.sqrt(math.fabs(x-1)),math.e))-(pow(5,1/math.fabs(5)))
-print(a)
-print(b)
-#task2
+
+
+def task1():
+    x = 1
+    y = 8
+    a = (math.sin(x) ** 2) + (math.cos(y) ** 2) + (math.log(math.e ** x + math.e ** (-y)))
+    b = (math.log(math.sqrt(abs(x - 1)))) - (pow(5, 1 / abs(5)))
+    print("Ответ:")
+    print('a =', a)
+    print('b =', b)
+
+
+def task2():
+    a = 1
+    b = -2
+    x = float(input('x = '))
+    y = (math.sqrt(x + a)) + ((x ** 2 + b) / x)
+    print('x =', y)
+
+
+def task3():
+    x = float(input('x = '))
+    y =math.sin(math.sqrt(x)) + math.cos(math.sqrt(x))
+    print('x =', y)
+
+
+def task4():
+    
